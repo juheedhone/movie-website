6e9db543973fabd19e36ba1adb3728eecda5be22
juheedhone <juheedhone12@gmail.com>
2025-08-09 20:02:11 +0530

    task:create project

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import { useEffect, useState } from "react";
-
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been Liked : ${hasLiked} `);
-  });
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
-
 const App = () => {
   return (
-    <div className="card-container">
-      <h2>Function arrow component</h2>
-      <Card title="star war" />
-      <Card title="avatar" />
-      <Card title="lion " />
-    </div>
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the hassle
+          </h1>
+        </header>
+      </div>
+    </main>
   );
 };
 
